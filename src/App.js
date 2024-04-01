2234bbcfa2954b465847f5ae9558c41066e17f0a
silizard <wkbkmj1@naver.com>
2024-04-01 13:41:18 +0900

    ToDoList #2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
     setToDo("");
     setToDos((currentArray) => [toDo, ...currentArray]);
   };
-  console.log(toDos);
   return (
     <div>
       <h1>My To Dos : {toDos.length}</h1>
@@ -23,6 +22,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
